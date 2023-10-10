cc71924f60079782d84cdee72795c93f11697e66
Carol A. Nikolic <carolnikolic@gmail.com>
2023-10-10 11:02:16 +0200

    feat: create cardInputFields object

---
+++ src/assets/objects/cardInputFields.js
@@ -0,0 +1,42 @@
+const cardInputFields = [
+    {
+        labelName: "Cardholder name",
+        placeholder: "Jane Appleseed",
+        value: "",
+        validation: /^[A-Za-z\s.'-]+$/,
+        validationMessage: "Please, enter a valida cardholder name."
+    },
+
+    {
+        labelCardNumber: "Card number",
+        placeholder: "1234 5678 9123 0000",
+        value: "",
+        validation: /^\d{16}$/,
+        validationMessage: "Please enter a valid 16-digit card number."
+    },
+    
+    {
+        labelDate: "Exp. Date (mm/yy)",
+        placeholder: "mm",
+        value: "",
+        validation: /^(0[1-9]|1[0-2])$/,
+        validationMessage: "Please enter a valid month between 01 and 12."
+    },
+    {
+        labelDate: "",
+        placeholder: "yy",
+        value: "",
+        validation: /^(0[1-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|6[0-9]|7[0-9]|8[0-9]|9[0-9])$/,
+        validationMessage: "Please enter a valid 2-digit expiration year.."
+    },
+
+    {
+        labelCVC: "cvc",
+        placeholder: "e.g. 123",
+        value: "",
+        validation: /^[0-9]{3}$/,
+        validationMessage: "Please enter a 3-digit CVC."
+    }
+]
+
+export default cardInputFields
