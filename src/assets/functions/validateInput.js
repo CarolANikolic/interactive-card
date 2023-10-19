954a4e32d2830d75956bf5eca48b2316345c9ccd
Carol A. Nikolic <carolnikolic@gmail.com>
2023-10-19 13:01:34 +0200

    refactor(validateInput): improve variable names and add a reson for separated statement for month and year inputs

--- src/assets/functions/validateInput.js
+++ src/assets/functions/validateInput.js
@@ -3,6 +3,8 @@ import applyValidationStyle from "./applyValidationStyle.js";
 import removeValidationStyle from "./removeValidationStyle.js";
 
 const validateInput = (inputArray, objectArray) => {
+
+    const emptyInputErrorMessage = "Can't be empty."
     let isValid = true;
     let monthValue = "";
     let yearValue = "";
@@ -10,29 +12,31 @@ const validateInput = (inputArray, objectArray) => {
     inputArray.each((i, input) => {
         const inputValue = $(input).val();
         const inputValidationExpression = objectArray[i].validation;
-        const inputValidationMessage = objectArray[i].validationMessage;
+        const validationErrorMessage = objectArray[i].validationMessage;
         const inputContainer = $(input).parent();
         const inputKeyword = objectArray[i].keyword;
 
         inputContainer.find("span").remove();
     
         if (inputValue === "") {
-            const emptyErrorMessage = $("<span>Can't be empty.</span>");
+            const emptyErrorMessage = $(`<span>${emptyInputErrorMessage}</span>`);
             inputContainer.append(emptyErrorMessage);
             isValid = false;
 
         } else if (!inputValidationExpression.test(inputValue)) {
             inputContainer.find("span").remove();
-            const validationMessage = $(`<span>${inputValidationMessage}</span>`);
+            const validationMessage = $(`<span>${validationErrorMessage}</span>`);
             inputContainer.append(validationMessage)
-            isValid = false;   
+            isValid = false;  
 
+            //Month and year in the card are in one span.
+            //We need to capture their value separated to combine it later and insert it into the card.
         } else if (inputKeyword === "month" && isValid) {
             monthValue = inputValue;
 
         } else if (inputKeyword === "year" && isValid) {
             yearValue = inputValue
-
+            
         } else {
             isValid = true;  
             insertInputValueIntoCard(inputKeyword, inputValue)   
